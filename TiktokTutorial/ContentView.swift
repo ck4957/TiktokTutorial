9f08b6a78080429ab5f490f5ca01ad77575a9cc7
Chirag Kular <kular_chirag@yahoo.com>
2025-02-06 16:48:49 -0500

    Initial Commit

---
+++ TiktokTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TiktokTutorial
+//
+//  Created by Chirag Kular on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
